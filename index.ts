b017de02a547a98a6917b6a21040c5f69b513564
monotera <nelson.mosquera@javeriana.edu.co>
2022-12-05 22:02:02 -0500

    Creates db connection with node server

--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
 import express, { Express, Request, Response } from 'express';
-import { Knex } from "knex";
+const { Client } = require('pg')
 import dotenv from 'dotenv';
-
 dotenv.config();
 
+//Database config.
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+})
+client.connect(function(err:Error) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
+
+//Express server config
 const app: Express = express();
 const port = process.env.PORT;
 
