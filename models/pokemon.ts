bdb2c9b34d9c5239041c52782907053fe440c50e
monotera <nelson.mosquera@javeriana.edu.co>
2022-12-15 02:50:50 -0500

    Fixes minor issues

--- models/pokemon.ts
+++ models/pokemon.ts
@@ -1,29 +1,29 @@
-const { DataTypes } = require("sequelize");
+const {DataTypes} = require("sequelize");
 const sequelize = require("../util/database");
 
 const Pokemon = sequelize.define(
-  "pokemon",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+    "pokemon",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        weight: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        is_male: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    weight: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    is_male: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-  },
-  { timestamps: false }
+    {timestamps: false}
 );
 
 module.exports = Pokemon;
--- util/database.ts
+++ util/database.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_URL);
 
